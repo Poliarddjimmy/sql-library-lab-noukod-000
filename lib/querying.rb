09d2e515a50d36e5bc8899173d54ab1083741a40
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-05-16 14:54:42 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,7 +15,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "select species, max(count(species)) form characters;"
+  "select species, count(species) form characters ORDER BY max(count(species)) limit 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
