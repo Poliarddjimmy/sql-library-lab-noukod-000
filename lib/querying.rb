d1402bc1bf5ced35de2d0b584f56eaf0194c1a0d
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-05-16 15:05:09 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,10 +19,10 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "select authors.name, series.name 
+  "select authors.name, subgenres.name 
   from authors
   Join series on series.author_id = authors.id 
-  join subgenres on subgenre.id = series.subgenre_id;"
+  join subgenres on subgenres.id = series.subgenre_id;"
 end
 
 def select_series_title_with_most_human_characters
