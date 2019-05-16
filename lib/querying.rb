35b4b480b673bcd411739b628f83d284d029b49b
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-05-16 14:28:05 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -3,7 +3,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
     select books.title, books.year
     from books
     inner join series
-    on books.id = series.book_id 
+    on books.series_id = series.id 
     where series.id = 1
     order by books.year asc ;
   "
