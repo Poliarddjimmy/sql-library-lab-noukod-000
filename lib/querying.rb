455633389df47f3bdfff37c69e4e1cb30193d636
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-05-16 14:57:38 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,7 +15,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "select species, max(total) from (select species, count(*) as total from characters order by species);"
+  "select species, max(total) from (select species, count(*) as total from characters group by species);"
 end
 
 def select_name_and_series_subgenres_of_authors
